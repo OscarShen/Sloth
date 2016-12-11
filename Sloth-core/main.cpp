b508e920325429c21b343f29c5ccf94a77ec3673
oscar <oscarshen09@foxmail.com>
2016-12-11 16:22:51 +0800

    main

--- Sloth-core/main.cpp
+++ Sloth-core/main.cpp
@@ -1,25 +1,38 @@
 #include "src/graphics/window.h"
-#include "src/utils/fileutils.h"
 #include "src/graphics/shader.h"
+#include "src/graphics/buffers/buffer.h"
+#include "src/graphics/buffers/vertex_array.h"
+#include "src/graphics/buffers/index_buffer.h"
+#include "src/utils/error_check.h"
+#include "src/graphics/simple_2d_renderer.h"
+
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-int main() 
+using namespace sloth;
+using namespace graphics;
+using namespace util;
+int main()
 {
-	using namespace sloth;
-	using namespace graphics;
 
 	Window window("Sloth!", 800, 600);
-	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
-
-	Shader shader;
-	shader.Compile("src/shader/basic.vert", "src/shader/basic.frag");
-
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	Shader shader("src/shader/basic.vert", "src/shader/basic.frag");
+	Renderable2D sprite(glm::vec3(5.0f, 5.0f, 0.0f), glm::vec2(4.0f, 4.0f), glm::vec4(1.0f, 0.0f, 1.0f, 1.0f), &shader);
+	Simple2DRender render;
+	double x, y;
+	shader.use();
+	glm::mat4 projection = glm::ortho(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+	shader.SetMatrix4("projection", projection, GL_TRUE);
 	while (!window.closed())
 	{
 		window.clear();
+		window.getCursorPos(x, y);
+		shader.SetVector2f("light_pos", (GLfloat)(x), (GLfloat)((600 - y)));
+		render.sumbit(&sprite);
+		render.flush();
 		window.update();
 	}
 }
